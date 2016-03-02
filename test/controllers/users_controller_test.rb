0eaee68920220d403f7d035c3ab3f0fe20f674f7
nexusrj <nexusgrj@icloud.com>
2016-03-02 18:20:12 +0800

    add layout

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -29,4 +29,13 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password: 'password',
+                                            password_confirmation: 'password',
+                                            admin: false}
+    assert_not @other_user.false.admin?
+  end
+
 end
